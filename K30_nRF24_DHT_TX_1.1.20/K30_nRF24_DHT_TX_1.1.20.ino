c6af062bafbcd129190a838d4f99b25d119974bc
jrom876 <jrom876@gmail.com>
2025-04-20 07:31:48 -0600

    Update K30_nRF24_DHT_TX_1.1.20.ino

--- K30_nRF24_DHT_TX_1.1.20/K30_nRF24_DHT_TX_1.1.20.ino
+++ K30_nRF24_DHT_TX_1.1.20/K30_nRF24_DHT_TX_1.1.20.ino
@@ -7,7 +7,7 @@
  *     Arduino MEGA, UNO or NANO
  *     
  *     Written by:   Jacob Romero  
- *     email:        admin@jrom.io 
+ *     email:        jrom876@gmail.com 
  *     Creative Engineering Solutions LLC 
  *     
  *   This module is the stable version of the transmitter (TX) in a 
